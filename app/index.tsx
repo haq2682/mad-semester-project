581e533cb29f2dc76e38785ccbeadecd9b0258a8
haq2682 <haq2682@gmail.com>
2024-11-07 17:57:40 +0500

    Initializing Project

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,20 @@
 import { Stack, Link } from 'expo-router';
-
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import { Button, Text, View } from 'tamagui';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
+      {/* <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home" />
         <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
           <Button title="Show Details" />
         </Link>
-      </Container>
+      </Container> */}
+      <Stack.Screen options={{ headerShown: false }} />
+      <View>
+        <Text>Hello World</Text>
+      </View>
     </>
   );
 }
